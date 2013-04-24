2a2da08bae256632ca064e2c9fd98146c76ede87
Max Whitney <mwhitney@cs.sfu.ca>
2013-04-23 22:25:55 -0700

    Fixes to facets, in selection marking, search error marking, and the loading indicator; some code style improvements.

--- frontend/facet.js
+++ frontend/facet.js
@@ -6,8 +6,8 @@ function setupFacet(container, globalQuery, name, view, makeConstraint) {
 	var clearElt = $("<button type=\"button\" class=\"btn btn-block btn-mini btn-warning\">Clear selection</button></ul>").appendTo(topBoxElt);
 	var searchBoxElt = $("<form class=\"searchbox\"></form>").appendTo(topBoxElt);
 	var searchBtnElt = $("<button type=\"submit\" class=\"btn btn-primary btn-link\"></button>").appendTo(searchBoxElt);
-	var searchElt = $("<input type=\"text\" autocomplete=\"off\" data-provide=\"typeahead\"></input>").appendTo($("<div class=\"inputbox\"></div>").appendTo(searchBoxElt));
-	var search = searchElt.typeahead();
+	var searchInputElt = $("<input type=\"text\" autocomplete=\"off\" data-provide=\"typeahead\"></input>").appendTo($("<div class=\"inputbox\"></div>").appendTo(searchBoxElt));
+	var search = searchInputElt.typeahead();
 
 	var listBoxElt = $("<div class=\"listbox\"></div>").appendTo(facetElt);
 	var loadingElt = makeLoadingIndicator().prependTo(listBoxElt);
@@ -21,15 +21,32 @@ function setupFacet(container, globalQuery, name, view, makeConstraint) {
 	$(window).resize(fit);
 	fit();
 
+	function setLoadingIndicator(enabled) {
+		if (enabled)
+			loadingElt.css('display', '');
+		else
+			loadingElt.css('display', 'none');
+	}
+	setLoadingIndicator(false);
+
+	function setSearchErrorStatus(isError) {
+		if (isError)
+			searchInputElt.addClass('error');
+		else
+			searchInputElt.removeClass('error');
+	}
+
 	var selectedValue = null;
 	var constraint = new Constraint();
 	globalQuery.addConstraint(constraint);
 	function select(value) {
 		selectedValue = value;
-		var cnstrVal = makeConstraint(value);
-		cnstrVal.name = name + ": " + value;
-		constraint.set(cnstrVal);
-		globalQuery.update();
+		if (value != null) {
+			var cnstrVal = makeConstraint(value);
+			cnstrVal.name = name + ": " + value;
+			constraint.set(cnstrVal);
+			globalQuery.update();
+		}
 	}
 
 	var curData = null;
@@ -48,48 +65,46 @@ function setupFacet(container, globalQuery, name, view, makeConstraint) {
 			});
 		}
 		curData = values;
-		searchElt.val("");
+		searchInputElt.val("");
+		setSearchErrorStatus(false);
 		if (values != null) {
-			searchElt.removeAttr('disabled');
+			searchInputElt.removeAttr('disabled');
 			search.data('typeahead').source = keyList(values);
 			for (value in values)
 				addValue(value, values[value]);
 		} else {
-			searchElt.removeAttr('data-source');
-			searchElt.attr('disabled', 'disabled');
+			searchInputElt.removeAttr('data-source');
+			searchInputElt.attr('disabled', 'disabled');
 			listElt.find('li').remove();
 		}
 	}
 	setData(null);
 
-	function setLoadingIndicator(enabled) {
-		if (enabled)
-			loadingElt.show();
-		else
-			loadingElt.hide();
-		loadingElt.attr('display', 'hidden');
-	}
-	setLoadingIndicator(false);
-
-	clearElt.click(function() {
+	clearElt.click(function () {
 		constraint.clear();
 		globalQuery.update();
 	});
-	globalQuery.onChange(function() {
+	globalQuery.onChange(function () {
 		setData(null);
+		setLoadingIndicator(true);
+	});
+	constraint.onChange(function (changeType) {
+		if (changeType == 'removed')
+			select(null);
 	});
 	globalQuery.onResult({
 		counts: view
 	}, function(result) {
+		setLoadingIndicator(false);
 		setData(result.counts.counts);
 	});
 	searchBoxElt.submit(function () {
-		var value = searchElt.val();
+		var value = searchInputElt.val();
 		if (curData != null && value in curData) {
-			searchElt.removeClass('error');
+			setSearchErrorStatus(false);
 			select(value);
 		} else
-			searchElt.addClass('error');
+			setSearchErrorStatus(true);
 		return false;
 	});
 }
