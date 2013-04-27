8806ad1b554e6970579d5f74e04271547d0cdeca
Max Whitney <mwhitney@cs.sfu.ca>
2013-04-27 15:23:09 -0700

    Changed facets to use context from other constraints.

--- frontend/facet.js
+++ frontend/facet.js
@@ -36,8 +36,31 @@ function setupFacet(container, globalQuery, name, view, makeConstraint) {
 			searchInputElt.removeClass('error');
 	}
 
+	var selectedValue = null;
+	var constraint = new Constraint();
+	globalQuery.addConstraint(constraint);
+	var ownCnstrQuery = new Query(globalQuery.backendUrl());
+	ownCnstrQuery.addConstraint(constraint);
+	var contextQuery = new Query(globalQuery.backendUrl(), 'setminus', globalQuery, ownCnstrQuery);
+	function select(value) {
+		setClearEnabled(value != null);
+		selectedValue = value;
+		if (value != null) {
+			var cnstrVal = makeConstraint(value);
+			cnstrVal.name = name + ": " + value;
+			constraint.set(cnstrVal);
+			listBoxElt.addClass('selected');
+			globalQuery.update();
+		} else {
+			listBoxElt.removeClass('selected');
+		}
+	}
+	function haveSelection() {
+		return selectedValue != null;
+	}
+
 	var curData = null;
-	function setData(data, onClick, itemClass) {
+	function setData(data) {
 		function getSortedValues() {
 			var sortedValues = [];
 			for (value in data)
@@ -52,13 +75,12 @@ function setupFacet(container, globalQuery, name, view, makeConstraint) {
 			return list;
 		}
 		function addValue(value, count) {
-			var classStr = value == itemClass != null ? " class=\"" + itemClass + "\"" : "";
+			var classStr = value == selectedValue ? " class=\"selected\"" : "";
 			var countStr =  count == null ? "" : " [" + count + "]";
 			var itemElt = $("<li" + classStr + ">" + value + countStr + "</li>").appendTo(listElt);
-			if (onClick != null)
-				itemElt.click(function() {
-					onClick(value, data[value]);
-				});
+			itemElt.click(function() {
+				select(value);
+			});
 		}
 		curData = data;
 		searchInputElt.val("");
@@ -79,34 +101,6 @@ function setupFacet(container, globalQuery, name, view, makeConstraint) {
 	}
 	setData(null);
 
-	function setSelectedData(value) {
-		var data = {};
-		data[value] = null;
-		setData(data, null, "selected");
-	}
-
-	var selectedValue = null;
-	var constraint = new Constraint();
-	globalQuery.addConstraint(constraint);
-	function select(value, count) {
-		setClearEnabled(value != null);
-		selectedValue = value;
-		if (value != null) {
-			var cnstrVal = makeConstraint(value);
-			cnstrVal.name = name + ": " + value;
-			constraint.set(cnstrVal);
-			globalQuery.update();
-			setSelectedData(value);
-		}
-	}
-	function haveSelection() {
-		return selectedValue != null;
-	}
-
-	function setQueryData(data) {
-		setData(data, select);
-	}
-
 	clearElt.click(function () {
 		constraint.clear();
 		globalQuery.update();
@@ -115,6 +109,19 @@ function setupFacet(container, globalQuery, name, view, makeConstraint) {
 		if (!haveSelection()) {
 			setData(null);
 			setLoadingIndicator(true);
+		}
+	});
+	contextQuery.onChange(function () {
+		if (haveSelection()) {
+			setData(null);
+			setLoadingIndicator(true);
+		}
+	});
+	ownCnstrQuery.onChange(function () {
+		// If our own constraint changes we don't get any new result,
+		// but still want to change the selection.
+		if (haveSelection()) {
+			setData(curData);
 		}
 	});
 	constraint.onChange(function (changeType) {
@@ -126,9 +133,19 @@ function setupFacet(container, globalQuery, name, view, makeConstraint) {
 	}, function(result) {
 		if (!haveSelection()) {
 			setLoadingIndicator(false);
-			setQueryData(result.counts.counts);
+			setData(result.counts.counts);
 		}
 	});
+	contextQuery.onResult({
+		counts: view
+	}, function(result) {
+		if (haveSelection()) {
+			setLoadingIndicator(false);
+			if (!(selectedValue in result.counts.counts))
+				result.counts.counts[selectedValue] = 0;
+			setData(result.counts.counts);
+		}
+	}, haveSelection);
 	searchBoxElt.submit(function () {
 		if (!haveSelection()) {
 			var value = searchInputElt.val();
