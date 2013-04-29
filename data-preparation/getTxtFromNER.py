81d6d1a17bace977ffe80dd9b74b103a19dc51a4
Maryam <msiahban@sfu.ca>
2013-04-29 00:40:03 -0700

    fixing some bugs in preparing json file

--- data-preparation/getTxtFromNER.py
+++ data-preparation/getTxtFromNER.py
@@ -1,56 +1,24 @@
 import sys
 import re
 
-def readNEROutput(fileName):
-        inpFile = open(fileName, "r")
-        #outFile = open("eventDescription.tok", "w")
-        outFile = open(fileName+".tok", "w")
-        locFile = open("locFromNER.txt", "w")
-
-        outputLine = ''
-        for line in inpFile:
-                if "PAR_SPLITTER_SYMBOL" in line: #or "PAR_SPLITTER_SYMBOL" in line:
-                        outputLine += "\n"
-                        outFile.write(outputLine)
-                        outputLine = ''
-                        continue
-
-                words = re.sub(r'\s', ' ', line).split()
-		location = []
-                for w in words:
-                        w_parts = w.split('/')
-                        outputLine += w_parts[0]+' '
-                        if w_parts[-1] == 'LOCATION':
-				location.append(w_parts[0])
-			elif len(location) > 0:
-                                print >> locFile, " ".join(location)
-				location = []
-
-        outputLine += "\n"
-        outFile.write(outputLine)
-
-        outFile.close
-	locFile.close
-        return fileName+".ner"
-
-
 
 def removeNERTags(fileName):
         inpFile = open(fileName, "r")
         #outFile = open("eventDescription.tok", "w")
         outFile = open(fileName+".tok", "w")
-        locFile = open("locFromNER.txt", "w")
+        locFile = open(sys.argv[2], "w")
 
         outputLine = ''
         for line in inpFile:
+                line = re.sub(r'\\/', '/', line)
                 words = re.sub(r'\s', ' ', line).split()
                 location = []
 		lineWords = []
                 for w in words:
                         w_parts = w.split('/')
-                        lineWords.append(w_parts[0])
+                        lineWords.append("/".join(w_parts[:-1]))
                         if w_parts[-1] == 'LOCATION':
-                                location.append(w_parts[0])
+                                location.append("/".join(w_parts[:-1]))
                         elif len(location) > 0:
                                 print >> locFile, " ".join(location)
                                 location = []
--- data-preparation/prepareJson.py
+++ data-preparation/prepareJson.py
@@ -4,6 +4,7 @@ import json
 import copy
 import codecs
 import stredit
+import os
 
 frameLabel = {}
 eventsLst = []
@@ -18,6 +19,7 @@ class Sentence:
 		self.predicates = [{} for i in range(num_predicates)]
 		self.wordList = []
 		self.sentence = sentence
+		self.processed = 0
 	
 	def processing_word(self, wordInf, wordIndex):
 		self.wordList.append(wordInf[0])
@@ -56,6 +58,7 @@ class Sentence:
 				
 
 	def process_sent(self):
+		if self.processed: return
 		global frameLabel
 		self.sentence = " ".join(self.wordList)
 		for predDic in self.predicates:
@@ -64,7 +67,9 @@ class Sentence:
 				exit(1)
 			v = predDic['V'][2]
 			for key in predDic.keys():
-				if key == 'V': continue
+				if key == 'V': 
+					predDic['V'] = (predDic['V'][1], v, predDic['V'][0])
+					continue
 				if v not in frameLabel: v = "defPred"
 				if key not in frameLabel[v]:	
 					if key in frameLabel["defPred"]:
@@ -76,7 +81,7 @@ class Sentence:
 				t = predDic[key][0]
 				lexical = " ".join(self.wordList[t[0]:t[1]])
 				predDic[key] = (lexical, role, t)
-			predDic['V'] = (predDic['V'][1], v, predDic['V'][0])
+		self.processed = 1
 
 
 def read_sentence(inFile):
@@ -104,7 +109,7 @@ def read_word(inFile):
 	return words
 
 def print_doc(docs, docId, outFile):
-	global eventsLst
+	global eventsLst, nerLst
 	tokDesc = " ".join([s.sentence for s in docs])
 	setTokDesc = set(tokDesc.split())
 	setDesc = set(eventsLst[docId]["description"].split())
@@ -115,6 +120,38 @@ def print_doc(docs, docId, outFile):
 		print len(setDesc & setTokDesc), setDesc & setTokDesc
 		exit(1)
 	Desc = eventsLst[docId]["description"]	
+	if len(nerLst[docId][0]) > 0:
+		eventsLst[docId]['person'] = {}
+		for l in nerLst[docId][0]:
+			b = tokDesc.find(" "+l+" ")
+			if b < 0 and tokDesc.startswith(l+" "):
+				b = 0
+			elif b < 0 and tokDesc.endswith(" "+ l):
+				b = tokDesc.find(" "+l)
+			elif b < 0:
+				print "err: '%s'  is not in '%s'" %(l, tokDesc)
+				exit(1)
+			e = b+len(l)
+			b += 1
+			(rb, re) = stredit.streditmap(Desc,tokDesc,b,e)
+			eventsLst[docId]['person'][l] = (rb, re)
+
+	if len(nerLst[docId][1]) > 0:
+		eventsLst[docId]['organization'] = {}
+		for l in nerLst[docId][1]:
+			b = tokDesc.find(" "+l+" ")
+			if b < 0 and tokDesc.startswith(l+" "):
+				b = 0
+			elif b < 0 and tokDesc.endswith(" "+ l):
+				b = tokDesc.find(" "+l)
+			elif b < 0:
+				print "err: '%s'  is not in '%s'" %(l, tokDesc)
+				exit(1)
+			e = b+len(l)
+			b += 1
+			(rb, re) = stredit.streditmap(Desc,tokDesc,b,e)
+			eventsLst[docId]['organization'][l] = (rb, re)
+
 	for sentInd, sent in enumerate(docs):
 		preSentLen = len(" ".join([s.sentence for s in docs[:sentInd]]))
 		#print "pre sent lens: ", preSentLen
@@ -137,7 +174,6 @@ def print_doc(docs, docId, outFile):
 						exit(1)
 					(rb, re) = stredit.streditmap(Desc,tokDesc,b,e)
 					text = Desc[rb:re]
-					#print text,"|", tokDesc[b-1:e],"|"
 					if key == "V":
 						printable["event"] = ((rb, re), text)
 						printable["eventRoot"] = pred[key][1]
@@ -150,6 +186,7 @@ def print_doc(docs, docId, outFile):
 				#	print "err,   multiple key!"
 				#	exit(1)
 				printable[key] = copy.deepcopy(eventsLst[docId][key])
+
 			print >> outFile, json.dumps(printable)
 			del printable
 
@@ -187,7 +224,7 @@ def read_fullEvents(fileName):
 					dict["locations"].append(loc)
 		dict["urls"]=urlDic
 		for key in locDict:
-			if key in items[2]:
+			if " "+key+" " in items[2] or items[2].startswith(key+" ") or items[2].endswith(" "+key) or items[2]==key:
 				loc = {}
 				loc["latitude"] = locDict[key][0]
 				loc["longtitude"] = locDict[key][1]
@@ -222,8 +259,7 @@ def read_fullEvents(fileName):
 
 def parse_SRL():
 	inFile = open(sys.argv[1], "r")
-	outFile = codecs.open(sys.argv[4], "w", "utf-8")
-	#outFile = open("events.jason", "w")
+	outFile = codecs.open(sys.argv[5], "w", "utf-8")
 	global eventsLst, locDict, noEvents
         docId=0
 	sentId=0
@@ -277,7 +313,7 @@ def parse_SRL():
 
 def read_frames(fileName):
 	inFile = open(fileName, "r")
-	global frameLabel
+	global frameLabel, script_dir
 	for line in inFile:
 		items = line[:-1].split("\t")
 		pred = items[2]
@@ -287,7 +323,7 @@ def read_frames(fileName):
 			frameLabel[pred] = {}
 		frameLabel[pred][arg] = role
 	inFile.close
-	inFile = open("def_roles.txt", "r")
+	inFile = open(script_dir+"def_roles.txt", "r")
 	frameLabel["defPred"] = {}
 	for line in inFile:
 		items = line[:-1].split("\t")
@@ -309,14 +345,66 @@ def read_location(fileName):
 		if len(items) > 2:
 			countryDict[key] = items[2]
 	inFile.close
-		
+	
+def readNER(fileName):
+        inpFile = open(fileName, "r")
+	global nerLst
+	nerLst = []
+        personLst = []
+	orgLst = []
+	locationLst = []
+	wLst = []
+	preTag = ''
+        for line in inpFile:
+                if "PAR_SPLITTER_SYMBOL" in line: #or "PAR_SPLITTER_SYMBOL" in line:
+			nerLst.append((personLst,orgLst,locationLst))
+                	personLst = []
+			orgLst = []
+			locationLst = []
+                        continue
+                line = re.sub(r'\\/', '/', line)
+                words = re.sub(r'\s', ' ', line).split()
+                for w in words:
+                        w_parts = w.split('/')
+                        if preTag != w_parts[-1] and preTag == 'PERSON':
+                                personLst.append(" ".join(wLst))
+			#	print personLst[-1], preTag 
+				preTag = w_parts[-1]
+				wLst = []
+                        elif preTag != w_parts[-1] and preTag == 'ORGANIZATION':
+                                orgLst.append(" ".join(wLst))
+			#	print orgLst[-1], preTag 
+				preTag = w_parts[-1]
+				wLst = []
+                        elif preTag != w_parts[-1] and preTag == 'LOCATION':
+                                locationLst.append(" ".join(wLst))
+			#	print locationLst[-1] , preTag 
+				preTag = w_parts[-1]
+				wLst = []
+			#elif preTag == w_parts[-1]:
+                        #        wLst.append(w_parts[0])
+			elif preTag != w_parts[-1]: 
+				preTag = w_parts[-1]
+				wLst = []
+                        wLst.append("/".join(w_parts[:-1]))
+
+
+	#if len(locationLst) > 0 or len(personLst) > 0 or len(orgLst) > 0:
+	nerLst.append((personLst,orgLst,locationLst))
+	print len(nerLst)
+        inpFile.close
+	
 
 if __name__ == '__main__':
-	if len(sys.argv) != 4:
+	if len(sys.argv) != 6:
 		print "usage:  python  %s <srl-data-file> <location-file> <crawled-data-file> <json-output-name>" %(sys.argv[0])
 		print "'frame_labels.txt' and 'def_roles.txt' are supposed to be in local directory."
 		exit(1)
-	read_frames("frame_labels.txt")
+	full_path = os.path.realpath(__file__)
+	global script_dir
+	script_dir = "/".join(full_path.split("/")[:-1]) + "/"
+	read_frames(script_dir+"frame_labels.txt")
 	read_location(sys.argv[2])
 	read_fullEvents(sys.argv[3])
+	readNER(sys.argv[4])
 	parse_SRL()
