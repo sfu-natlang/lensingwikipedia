7840f3a0f7a250522e351c2678f21414d27dd767
Max Whitney <mwhitney@cs.sfu.ca>
2013-05-04 13:01:08 -0700

    Facet choices in config file.

--- frontend/config.def.js
+++ frontend/config.def.js
@@ -2,3 +2,8 @@ backendUrl = "http://localhost:1500";
 baseWikipediaUrl = "https://en.wikipedia.org";
 minMapZoom = 1;
 maxMapZoom = 5;
+facets = {
+	"role": "Role (any arg)",
+	"Role (arg 1)": "Role (any arg)",
+	"locationText": "Location"
+};
