653d33bce7b711a3689f9ecbd3c884ec4ce76d8a
Max Whitney <mwhitney@alumni.sfu.ca>
2013-09-29 19:23:57 +0800

    Changed backend index comma replacement character to tab.
    Right now actual tabs in keyword input get changed to spaces irretrievably. I can't think why there would be any tabs in keywords, so this seems ok.

--- backend/whooshutils.py
+++ backend/whooshutils.py
@@ -5,20 +5,20 @@ Shared things and consistency settings.
 import re
 
 comma_char = ","
-comma_rep_char = "_"
+comma_rep_char = "\t"
 comma_re = re.compile(re.escape(comma_char))
-comma_rep_char_re = re.compile(re.escape(comma_rep_char))
+comma_rep_re = re.compile(re.escape(comma_rep_char))
 
 # Setting for the 'commas' option when making a schema keyword field
 keyword_field_commas = True
 
 # Join keywords for the value of a keyword field
 def join_keywords(keywords):
-  return comma_char.join(comma_re.sub("_", k.strip()) for k in keywords)
+  return comma_char.join(comma_re.sub(comma_rep_char, comma_rep_re.sub(" ", k.strip())) for k in keywords)
 
 # Escape keywords without merging them into a single field value
 def escape_keywords(keywords):
-  return (comma_re.sub("_", k.strip()) for k in keywords)
+  return (comma_re.sub(comma_rep_char, k.strip()) for k in keywords)
 
 # Separate the value of a keyword field into separate keywords
 def split_keywords(value):
@@ -26,6 +26,6 @@ def split_keywords(value):
     if len(value) == 0:
       return []
     else:
-      return [comma_rep_char_re.sub(comma_char, p) for p in value.split(comma_char)]
+      return [comma_rep_re.sub(comma_char, p) for p in value.split(comma_char)]
   else:
     return [value]
