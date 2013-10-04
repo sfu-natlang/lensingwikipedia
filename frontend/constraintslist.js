7e0cdfa1b7aafbf2721c7fd9762add6ac538f398
ramtinms <Mehdizadeh.ramtin@gmail.com>
2013-10-04 14:09:43 -0700

    Update constraintslist.js

--- frontend/constraintslist.js
+++ frontend/constraintslist.js
@@ -36,7 +36,7 @@ function setupConstraintList(container, globalQuery) {
 		cnstrTextElt.append(cnstr.name());
 		container.trigger('changedSize');
 		cnstrElt.click(function() {
-			cnstr.clear();
+			// cnstr.clear();  // just for test
 			globalQuery.update();
 			removeElement(cnstrElt);
 		});
