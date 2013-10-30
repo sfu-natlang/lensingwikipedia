f7c92d45a70b64b632ff40da063043f450efe02b
Max Whitney <mwhitney@alumni.sfu.ca>
2013-10-30 18:41:16 +0800

    Basic frontend text search.

---
+++ frontend/textsearch.js
@@ -0,0 +1,57 @@
+/*
+ * Text search control.
+ */
+
+/*
+ * Setup the control in some container element.
+ * container: container element as a jquery selection
+ * globalQuery: the global query
+ */
+function setupTextSearch(container, globalQuery) {
+	var outerElt = $("<div class=\"textsearch\"></div>").appendTo(container);
+
+	var searchInputElt = $("<input type=\"text\" title=\"Enter search term here.\"></input>").appendTo($("<div class=\"inputbox\"></div>").appendTo(outerElt));
+
+	var btnBoxElt = $("<div class=\"buttonbox\"></div>").appendTo(outerElt);
+	var clearElt = $("<button type=\"button\" class=\"btn btn-warning\" title=\"Clear the text search constraint.\">Clear</button>").appendTo(btnBoxElt);
+	var searchElt = $("<button type=\"button\" class=\"btn btn-primary\" title=\"Add text search constraint.\">Search</button>").appendTo(btnBoxElt);
+
+	$("<div class=\"infobox\">See the <a href=\"http://pythonhosted.org/Whoosh/querylang.html\" target=\"_blank\">Whoosh documentation</a> for help.</div>").appendTo(outerElt);
+
+	fillElement(container, outerElt, 'vertical');
+
+	var constraint = new Constraint();
+	globalQuery.addConstraint(constraint);
+
+	var currentSearchTerm = null;
+	function update(searchTerm) {
+		if (searchTerm == null) {
+			if (currentSearchTerm != null) {
+				constraint.clear();
+				clearElt.attr('disabled', 'disabled');
+				globalQuery.update();
+				currentSearchTerm = null;
+			}
+		} else {
+			searchTerm = $.trim(searchTerm);
+			if (searchTerm.length > 0) {
+				constraint.name("Text search: " + searchTerm);
+				constraint.set({
+					type: 'textsearch',
+					value: searchTerm
+				});
+				globalQuery.update();
+				clearElt.removeAttr('disabled');
+				currentSearchTerm = searchTerm;
+			}
+		}
+	}
+	update(null);
+
+	clearElt.bind('click', function() {
+		update(null);
+	});
+	searchElt.bind('click', function() {
+		update(searchInputElt.val());
+	});
+}
