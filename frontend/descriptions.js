ecadaf20c424ec0c6b6f9f2ebbf0e37ac7f5f045
Max Whitney <mwhitney@alumni.sfu.ca>
2013-12-03 19:53:16 +0800

    Factored out HTML description replacement code and merged some fixes and cleanup for it from the avherald branch.

---
+++ frontend/descriptions.js
@@ -0,0 +1,67 @@
+/*
+ * Things especially specific to the The Aviation Herald domain.
+ */
+
+/*
+ * Prepare replacements for an event structure to be used by the other functions.
+ */
+function prepareReplacements(event, extraReplacements) {
+	if (extraReplacements == null)
+		extraReplacements = [];
+	
+	descriptionReplacements = typeof(event.descriptionReplacements) == 'string' ?  JSON.parse(event.descriptionReplacements) : event.descriptionReplacements;
+
+	var replacements = [].concat(extraReplacements);
+	$.each(descriptionReplacements, function (itemText, itemInfo) {
+		$.each(itemInfo.span, function (spanId, span) {
+			var url = itemInfo.url;
+			if (url.lastIndexOf("http://", 0) !== 0)
+				url = baseWikipediaUrl + url;
+			var link = "<a href=\"" + url + "\">" + itemText + "</a>";
+			replacements.push({ src: itemText, dst: link, span: span });
+		});
+	});
+
+	replacements.sort(function (repA, repB) { return repA.span[0] - repB.span[0] });
+
+	return replacements;
+}
+
+// Apply replacements to text.
+// indexOffset: Index of the substring provided as text, relative to whatever longer string the spans are for.
+function replace(text, replacements, initIndexOffset) {
+	if (initIndexOffset == null)
+		initIndexOffset = 0;
+
+	function clean(text) {
+		var escapes = { '&': '&amp;', '<': '&lt;', '>': '&gt;' };
+		return text.replace(/\[[0-9]+\]|[&<>]/g, function (old) {
+			return escapes.hasOwnProperty(old) ? escapes[old] : "";
+		});
+	}
+
+	var lastEndIndex = 0,
+	    endIndexOffset = initIndexOffset + text.length,
+	    indexOffset = -initIndexOffset,
+	    seen = {};
+	$.each(replacements, function (id, item) {
+		var i = item.span[0], j = item.span[1];
+		if (i < initIndexOffset || j > endIndexOffset) {
+			// continue
+		} else if (seen.hasOwnProperty(item.src)) {
+			// continue
+		} else if (i < lastEndIndex) {
+			console.log("warning: span " + i + ":" + j + " \"" + item.src + "\" overlaps previous span, not making a link");
+		} else {
+			var oldLen = text.length;
+			text = text.substring(0, lastEndIndex + indexOffset) + clean(text.substring(lastEndIndex + indexOffset, i + indexOffset)) + item.dst + text.substring(j + indexOffset, text.length);
+			lastEndIndex = j;
+			indexOffset += text.length - oldLen;
+			seen[item.src] = true;
+		}
+	});
+
+	text = text.substring(0, lastEndIndex + indexOffset) + clean(text.substring(lastEndIndex + indexOffset, text.length));
+
+	return text;
+}
--- frontend/wikipediahistory.js
+++ frontend/wikipediahistory.js
@@ -9,37 +9,12 @@ function clearDescriptionList(listElt) {
 	listElt.find("dt,dd").remove();
 }
 function addToDescriptionList(descriptions, listElt) {
-	function formatDescription(text, replacements) {
-		function clean(text) {
-			var escapes = { '&': '&amp;', '<': '&lt;', '>': '&gt;' };
-			return text.replace(/\[[0-9]+\]|[&<>]/g, function (old) {
-				return escapes.hasOwnProperty(old) ? escapes[old] : "";
-			});
-		}
-
-		var replacementsOrder = [];
-		$.each(replacements, function (itemText, itemInfo) {
-			replacementsOrder.push(itemText);
+	// This is to handle data that contains a single span per item rather than a list
+	function handleSingleSpans(event) {
+		event.descriptionReplacements = JSON.parse(event.descriptionReplacements);
+		$.each(event.descriptionReplacements, function (itemText, itemInfo) {
+			itemInfo.span = [itemInfo.span];
 		});
-		replacementsOrder.sort(function (repA, repB) { return replacements[repA].span[0] - replacements[repB].span[0] });
-
-		var lastEndIndex = 0,
-		    indexOffset = 0;
-		$.each(replacementsOrder, function (id, itemText) {
-			var itemInfo = replacements[itemText];
-			var i = itemInfo.span[0], j = itemInfo.span[1];
-			if (i < lastEndIndex) {
-				console.log("warning: span " + i + ":" + j + " \"" + itemText + "\" overlaps previous span, not making a link");
-			} else if (itemInfo.hasOwnProperty('url')) {
-				var link = "<a href=\"" + baseWikipediaUrl + itemInfo.url + "\">" + itemText + "</a>";
-				var oldLen = text.length;
-				text = text.substring(0, lastEndIndex + indexOffset) + clean(text.substring(lastEndIndex + indexOffset, i + indexOffset)) + link + text.substring(j + indexOffset, text.length);
-				lastEndIndex = j;
-				indexOffset += text.length - oldLen;
-			}
-		});
-		text = text.substring(0, lastEndIndex + indexOffset) + clean(text.substring(lastEndIndex + indexOffset, text.length));
-		return text;
 	}
 
 	$.each(descriptions, function (i, event) {
@@ -49,7 +24,9 @@ function addToDescriptionList(descriptions, listElt) {
 		if (event.hasOwnProperty('eventRoot'))
 			tooltipText += ", predicate stem '" + event.eventRoot + "'";
 		tooltipText += ".";
-		var descHtml = formatDescription(event.description, JSON.parse(event.descriptionReplacements));
+		handleSingleSpans(event);
+		var replacements = prepareReplacements(event);
+		var descHtml = replace(event.description, replacements);
 		var descElt = $("<dt title=\"" + tooltipText + "\"><a href=\"" + yearUrl + "\">" + yearText + "</a></dt>" + "<dd>" + descHtml + "</dd>").appendTo(listElt);
 		descElt.find("a").attr('target', '_blank');
 	});
