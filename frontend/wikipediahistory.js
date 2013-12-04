8474df975e45ada4bcea13cea5270512f0530b60
Max Whitney <mwhitney@alumni.sfu.ca>
2013-12-04 09:17:58 +0800

    Sentence field and use of sentence field instead of full description in the frontend event listing.

--- frontend/wikipediahistory.js
+++ frontend/wikipediahistory.js
@@ -21,16 +21,34 @@ function addToDescriptionList(descriptions, listElt) {
 	}
 
 	$.each(descriptions, function (i, event) {
+		handleSingleSpans(event);
+		var replacements = prepareReplacements(event, baseWikipediaUrl);
 		var yearText = event.year > 0 ? event.year + " CE" : -event.year + " BCE";
+		var sentenceSpan = event.sentenceSpan.split(",");
+		var sentenceStartIndex = +sentenceSpan[0];
+
 		var yearUrl = baseWikipediaUrl + "/wiki/" + (event.year > 0 ? event.year : -event.year + "BC");
 		var tooltipText = "Event ID " + event.dbid + " in " + event.year;
 		if (event.hasOwnProperty('eventRoot'))
 			tooltipText += ", predicate stem '" + event.eventRoot + "'";
 		tooltipText += ".";
-		handleSingleSpans(event);
-		var replacements = prepareReplacements(event, baseWikipediaUrl);
-		var descHtml = replace(event.description, replacements);
-		var descElt = $("<dt title=\"" + tooltipText + "\"><a href=\"" + yearUrl + "\">" + yearText + "</a>: " + event.event + "</dt>" + "<dd>" + descHtml + "</dd>").appendTo(listElt);
-		descElt.find("a").attr('target', '_blank');
+
+		var shortDesc = replace(event.sentence, replacements, sentenceStartIndex);
+		var longDesc = replace(event.description, replacements);
+
+		var dtElt = $("<dt title=\"" + tooltipText + "\"><a href=\"" + yearUrl + "\">" + yearText + "</a>: " + event.event + "</dt>").appendTo(listElt);
+		var ddElt = $("<dd>" + shortDesc + "</dd>").appendTo(listElt);
+		dtElt.find("a").attr('target', '_blank');
+		ddElt.find("a").attr('target', '_blank');
+
+		// Expansion to long description
+		if (event.description != event.sentence) {
+			var expandFullElt = $("<span class=\"icon-plus\"></span>").appendTo(ddElt);
+			var fullElt = $("<div class=\"eventfulldescription\">" + longDesc + "</div>").appendTo(ddElt);
+			expandFullElt.click(function () {
+				$(this).toggleClass('icon-plus icon-minus');
+				fullElt.toggleClass('expanded');
+			});
+		}
 	});
 }
