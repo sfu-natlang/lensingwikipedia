e6e3511e6ba877a37b20be966d48d6619d729d02
Maryam <msiahban@sfu.ca>
2014-04-14 14:38:43 -0700

    update pipline scripts (adding organization spider and update reverse geocoding script)

--- data-preparation/dataPrepration.sh
+++ data-preparation/dataPrepration.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-W_DIR=/cs/natlang-projects/users/maryam/wikiCrawler/Crawl_20131102
+date=20140413                                                         #TODO: get date from machine
+W_DIR=/cs/natlang-projects/users/maryam/wikiCrawler/Crawl_$date
 NER_HOME=/cs/natlang-projects/users/maryam/clustering/NERStanford
 UIUC_Dir=/cs/natlang-sw/Linux-x86_64/NL/TOOLKITS/UIUCSRL/2.0/
 PYPATH=/cs/natlang-sw/Linux-x86_64/NL/LANG/PYTHON/2.7.3/bin/python
 CORENLP=/cs/natlang-projects/users/maryam/codes/stanford-corenlp-full-2013-06-20
 SCRIPT_PATH=/cs/natlang-projects/users/maryam/wikiCrawler
 
-DUMP_Dir=$W_DIR/Dump_20131102		#TODO: get date from machine
+DUMP_Dir=$W_DIR/Dump_$date		
 #module load NL/LANG/PYTHON/2.7.3
 #module load NL/TOOLKITS/UIUCSRL/2.0
 
@@ -17,7 +18,7 @@ cd $W_DIR
 ### STEP 1: crawling the main domain ###
 ########################################
 echo "Running eventSpider to crawl wiki articles"
-scrapy runspider -a outDir=$DUMP_Dir -a endYear='2013' $SCRIPT_PATH/spiders/eventSpider.py
+scrapy runspider -a outDir=$DUMP_Dir -a endYear='2014' $SCRIPT_PATH/spiders/eventSpiderSplit.py
 
 ### STEP 2: Running CORENLP ###
 ###############################
@@ -29,12 +30,12 @@ for dir in $(find $DUMP_Dir -mindepth 1 -type d); do
 	java -cp $CORENLP/stanford-corenlp-3.2.0.jar:$CORENLP/stanford-corenlp-3.2.0-models.jar:$CORENLP/xom.jar:$CORENLP/joda-time.jar:$CORENLP/jollyday.jar -Xmx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse,dcoref -filelist $dir/path.txt -outputDirectory $dir -outputExtension .xml -replaceExtension 
 done
 
-rm newJ.json
-rm outCoRef.txt
-rm NER.loc
-rm NER.per
-rm NER.tok
-rm NER.org
+#rm newJ.json
+#rm outCoRef.txt
+#rm NER.loc
+#rm NER.per
+#rm NER.tok
+#rm NER.org
 
 echo "Extracting locations recognized by NER (\"locFromNER.txt\") and tokenized descriptions (\"eventDescription.ner.tok\") to run SRL"
 for dir in $(find $DUMP_Dir -mindepth 1 -type d); do
@@ -42,10 +43,10 @@ for dir in $(find $DUMP_Dir -mindepth 1 -type d); do
 done
 
 
-#NER.tok:	each line is one event (tokenized)
+#NER.tok:		each line is one event (tokenized)
 #NER.loc: 		each line is a location predicted by NER
 #NER.per: 		each line is a person predicted by NER
-
+#NER.org: 		each line is a organization predicted by NER
 
 
 ### STEP 3: Extract location inforamtion ###
@@ -54,23 +55,29 @@ done
 cd $W_DIR
 
 echo "Running locationSpider to crawl all links and specified locations"
-scrapy runspider -a infile='newJ.json' -a outfile='url.json' -a outfileLoc='location.json' -a outfilePer='person.json' $SCRIPT_PATH/spiders/entitySpider.py
+scrapy runspider -a infile='newJ.json' -a outfile='url.json' -a outfileLoc='location.json' -a outfilePer='person.json' -a outfileOrg='organization.json' $SCRIPT_PATH/spiders/entitySpider.py
 echo "Running nerLocSpider to crawl corresponding wiki articles and recognize locations"
 scrapy runspider -a infile='NER.loc' -a outfile='locFromNER.json' $SCRIPT_PATH/spiders/nerLocSpider.py
 echo "Running nerPersonSpider to crawl corresponding wiki articles and recognize persons"
 scrapy runspider -a infile='NER.per' -a outfile='perFromNER.json' $SCRIPT_PATH/spiders/nerPersonSpider.py
+echo "Running nerOrgSpider to crawl corresponding wiki articles and recognize organizations"
+scrapy runspider -a infile='NER.org' -a outfile='orgFromNER.json' $SCRIPT_PATH/spiders/nerOrgSpider.py
 
 echo "Combining all location information (from NER and wiki links in descriptions)"
 $PYPATH $SCRIPT_PATH/makeLocationDict.py location.json locFromNER.json txturl2latlong.json
-#locationDictionary.txt: is a dictionary file from url or text to latitude longtitue. the format of each line is:
-#[text|url]\tlatitude , longtitude
+#locationDictionary.txt: is a dictionary file from url or text to latitude longtitue. each line is:
+#a json entry, keys: ["text" | "url"], "title", "latitude", "longtitude"
 
 echo "Combining all person information (from NER and wiki links in descriptions)"
 $PYPATH $SCRIPT_PATH/makePersonDict.py person.json perFromNER.json personDictionary.json
-#personDictionary.txt: is a dictionary file from url or text to name (of person). the format of each line is:
-#[text|url]\tPersonName
+#personDictionary.txt: is a dictionary file from url or text to name (of person). each line is:
+#a json entry, keys: ["text" | "url"], "title"
 
-exit
+echo "Combining all organization information (from NER and wiki links in descriptions)"
+$PYPATH $SCRIPT_PATH/makePersonDict.py organization.json orgFromNER.json organizationDictionary.json
+#personDictionary.txt: is a dictionary file from url or text to name (of organization). each line is:
+#a json entry, keys: ["text" | "url"], "title"
+
 
 ### STEP 4: Reverse Geocoding ###
 #################################
@@ -114,4 +121,4 @@ cd $W_DIR
 
 echo "Writing the final json file"
 
-$PYPATH prepareJson.py descriptions.srl locationDictionary.json personDictionary.json url.json newJ.json fullData.json
+$PYPATH $SCRIPT_PATH/prepareJson.py descriptions.srl locationDictionary.json personDictionary.json url.json newJ.json fullData.json
--- data-preparation/reverseGeocoding.sh
+++ data-preparation/reverseGeocoding.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PYPATH=/cs/natlang-sw/Linux-x86_64/NL/LANG/PYTHON/2.7.3/bin/python
+SCRIPTS=/cs/natlang-projects/users/maryam/wikiCrawler
 
 fileName=$1
 
@@ -11,12 +12,13 @@ nday=`echo "$nloc / $nquery" | bc`
 
 log=locFilesLog
 mkdir -p $log
+rm $log/*
 for (( i=0; i<$nday; i++ )); do
      let s="i * $nquery + 1"
      let t="( i + 1 ) * $nquery"
      sed -n "${s},${t}p" $fileName > $log/loc.$i
      lastInd=$i
-     $PYPATH reGeocoding.py $log/loc.$i $log/loc.$i.out
+     $PYPATH $SCRIPTS/reGeocoding.py $log/loc.$i $log/loc.$i.out
      echo "step $i" 
      let waiting="24 * 3600"
      sleep $waiting 
@@ -24,15 +26,13 @@ done
 lastInd=$i
 let tot="$nday * $nquery"
 let remaining="$nloc - $tot"
-let lastInd="$lastInd + 1"
-#echo $remaining
+#let lastInd="$lastInd + 1"
 if [ $remaining -gt 0 ]; then 
 	tail -$remaining $fileName > $log/loc.$lastInd 
 	let nday="$nday + 1"
-     	$PYPATH reGeocoding.py $log/loc.$lastInd $log/loc.$lastInd.out
+     	$PYPATH $SCRIPTS/reGeocoding.py $log/loc.$lastInd $log/loc.$lastInd.out
 fi
 #if [ $remaining -gt 0 ]; then let nday="$nday + 1"; fi
-let nday="$nday + 1"
 #echo $nday
 
 
