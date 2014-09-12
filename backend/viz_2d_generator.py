5bfa3b3c3ca6e805806db66b41879d8f86d995a3
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2014-09-12 02:30:45 -0700

    viz_2d_generator: Initial code to read sentence from index and call feature extractor.

---
+++ backend/viz_2d_generator.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python2
+
+"""
+Usage: %s [opts] WHOOSH-INDEX-DIR [OUTPUT-WHOOSH-INDEX-DIR]
+
+Arguments:
+WHOOSH-INDEX-DIR         Directory of the Whoosh index for the input data.
+OUTPUT-WHOOSH-INDEX-DIR  Directory of the Whoosh index for the output data.
+
+Generate 2D Visualization coordinates for data indexed in Whoosh.
+
+1. Read data from Whoosh and extract the required text.
+2. Perform feature extraction.
+3. Calculate 2d coordinates using BH-tSNE.
+4. Index data back into Whoosh.
+"""
+
+import sys
+import os, os.path
+import whoosh, whoosh.index
+import viz_feature_extractor
+import whooshutils
+
+
+def iter_events_from_index(index):
+    with index.searcher() as searcher:
+        for i, hit in enumerate(searcher.search(whoosh.query.Every(), limit=None)):
+            sentence = hit['sentence']
+            if sentence:
+                yield i, hit['id'], sentence
+
+
+def extract_features(data):
+    features = viz_feature_extractor.extract_features((sentence for (i, id, sentence) in data))
+    return features
+
+
+def run(input_index, output_index, doc_buffer_size, do_dummy):
+    data = iter_events_from_index(input_index)
+    features = extract_features(data)
+
+
+if __name__ == '__main__':
+    import getopt
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], ":Db:")
+        if len(args) not in [1, 2]:
+            raise getopt.GetoptError("wrong number of positional arguments")
+        opts = dict(opts)
+    except getopt.GetoptError:
+        print >> sys.stderr, __doc__.strip('\n\r') % (sys.argv[0])
+        sys.exit(1)
+
+    input_index_path = args[0]
+    output_index_path = args[1] if len(args) > 1 else None
+    do_dummy = '-D' in opts
+    doc_buffer_size = None if output_index_path is not None else (int(opts['-b']) if '-b' in opts else 1000)
+
+    if output_index_path is not None and not os.path.exists(output_index_path):
+        os.mkdir(output_index_path)
+    input_index = whoosh.index.open_dir(input_index_path)
+    output_index = (whoosh.index.create_in(output_index_path, input_index.schema.copy())
+                    if output_index_path is not None else input_index) if not do_dummy else None
+
+    run(input_index, output_index, doc_buffer_size, do_dummy)
+
+
+
+
