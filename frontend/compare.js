535822c268b580b03496f29ba14fdfa75fc2e2ed
Andrei Vacariu <andrei@avacariu.me>
2014-11-18 01:58:14 -0800

    Add UI elements to Compare tab.

--- frontend/compare.js
+++ frontend/compare.js
@@ -1,3 +1,5 @@
+var currentFacet;
+
 /*
  * Setup the control in some container element.
  * container: container element as a jquery selection
@@ -5,6 +7,41 @@
  * globalQuery: the global query
  */
 function setupCompare(container, globalQuery, facets) {
-	var outerElt = $('<div class="compare">Text here</div>').appendTo(container);
+	/*  BEGIN BUILD UI */
+
+	var outerElt = $('<div class="compare"></div>').appendTo(container);
+	var formElt = $("<form></form>").appendTo(outerElt);
+	var clearSelElt = $('<button type="button" class="btn btn-mini btn-warning clear mapclear" title="Clear">Clear selection</button>').appendTo(formElt);
+	var modeElt = $('<select class="btn btn-mini"></select>').appendTo(formElt);
+	var updateElt = $('<button type="submit" class="btn btn-warning" title="Update the visualization">Update</button></ul>').appendTo(formElt);
+
+	var loadingIndicator = new LoadingIndicator(outerElt);
+
+	function setLoadingIndicator(enabled) {
+		//svgElt.css('display', !enabled ? '' : 'none');
+		loadingIndicator.enabled(enabled);
+	}
+
+	$.each(facets, function (idx, facet) {
+		$('<option value="' + idx + '">' + facet.title + ' facet</option>').appendTo(modeElt);
+	});
+
 	fillElement(container, outerElt, 'vertical');
+
+	/* END BUILD UI */
+
+	/* BEGIN CALLBACKS */
+
+	updateElt.click(function(event) {
+		setLoadingIndicator(true);
+		currentFacet = facets[Number(modeElt.value())];
+		// TODO get all the values from the facet somehow
+	});
+
+	// disable form because we don't want to refresh the page
+	formElt.submit(function() {
+		return false;
+	});
+
+	/* END CALLBACKS */
 }
