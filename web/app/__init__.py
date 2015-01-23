1642aca158a532cd3c3c5ae9d2976dff09b910e1
Andrei Vacariu <andrei@avacariu.me>
2015-01-23 12:40:41 -0800

    Add migrations using Alembic.
    Details for use are in the README.md

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -2,11 +2,17 @@ from flask import Flask
 
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 lm = LoginManager()
 lm.init_app(app)
--- web/run.py
+++ web/run.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python2
 
-from app import app
-app.run(debug=True)
+from app import app, manager
+manager.run()
