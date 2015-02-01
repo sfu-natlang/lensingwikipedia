8ad86b06926bff28303b203fc104d90926fed3f6
Andrei Vacariu <andrei@avacariu.me>
2015-01-31 18:51:44 -0800

    Add the domain in the config file.
    This allows us to check which domain we're in and load js/css based on that.

--- web/config.py
+++ web/config.py
@@ -6,6 +6,9 @@ SECRET_KEY = 'something'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
+# override this in the local_config, but we need a default
+DOMAIN = "wikipediahistory"
+
 try:
     from local_config import *
 except ImportError:
