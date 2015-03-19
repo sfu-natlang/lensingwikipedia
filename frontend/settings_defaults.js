76cd0c95a860ffbcd2bfbb6f508c7e794a0efad8
Andrei Vacariu <andrei@avacariu.me>
2015-03-19 15:33:29 -0700

    Remove old frontend/

--- frontend/settings_defaults.js
+++
@@ -1,6 +0,0 @@
-/*
- * Default installation-specific Javascript settings.
- */
-
-// URL for the backend.
-FrontendConfig.backendUrl = "http://localhost:1500";
--- frontend/settings_defaults.mk
+++
@@ -1,7 +0,0 @@
-#
-# Default installation-specific settings.
-#
-# See the readme file for information about how the config files are used.
-#
-
-OWNJS:=$(OWNJS) frontendsettings.js
