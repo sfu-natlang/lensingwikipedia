643f18491b9f3dfdfece6c33d8547ddbc61f5bd3
Andrei Vacariu <andrei@avacariu.me>
2015-08-19 12:32:27 -0700

    Randomly generate CSRF key at runtime

--- web/config.py
+++ web/config.py
@@ -6,13 +6,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'something'
+SECRET_KEY = os.urandom(32)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 # override this in the local_config, but we need a default
 DOMAIN = "wikipediahistory"
 
+# XXX This will definitely need to be overridden in local_config.py
 BACKEND_URL = "http://localhost:1500"
 
 try:
