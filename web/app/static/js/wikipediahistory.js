a35e51dae58487d7ba23fc5d980be9844700918f
Andrei Vacariu <andrei@avacariu.me>
2015-08-28 14:31:04 -0700

    Revert "added glyphicon font files; added class name to plus/minus div"
    This reverts commit 8cf94a576bf3c958198626e9fae6cec834e0d619.

--- web/app/static/js/wikipediahistory.js
+++ web/app/static/js/wikipediahistory.js
@@ -74,7 +74,7 @@ function addToDescriptionList(descriptions, listElt) {
 
 		// Expansion to long description
 		if (event.description != event.sentence) {
-			var expandFullElt = $("<span class=\"glyphicon glyphicon-plus-sign\"></span>").appendTo(ddElt);
+			var expandFullElt = $("<span class=\"glyphicon-plus-sign\"></span>").appendTo(ddElt);
 			var fullElt = $("<div class=\"eventfulldescription\">" + longDesc + "</div>").appendTo(ddElt);
 			expandFullElt.click(function () {
 				$(this).toggleClass('glyphicon-plus-sign glyphicon-minus-sign');
