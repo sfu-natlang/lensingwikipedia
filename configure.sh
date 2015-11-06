7e4ba6e2afba6cea4ec03954c17bead0ae556706
Andrei Vacariu <andrei@avacariu.me>
2015-11-05 16:52:19 -0800

    Make configure.sh less fragile

--- configure.sh
+++ configure.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-source config.env
+set -e
+source ./config.env
 
-rm -rf build
-mkdir -p build/index
+if [ -z "${INDEX_PATH}" ]; then
+    echo "INDEX_PATH is not set (or blank)"
+    echo "Please set it in config.env"
+    exit 1
+fi
+
+rm -rf ./build
+mkdir -p ./build/index
 
 echo "Copying index to build/index"
-cp -r "${INDEX_PATH}"/* build/index
+cp -r "${INDEX_PATH}"/* ./build/index
