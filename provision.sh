ba6a57becd30adb35e069838e35ddd2b43210a74
Andrei Vacariu <andrei@avacariu.me>
2015-11-08 18:21:16 -0800

    Fix issues with provision.sh

--- provision.sh
+++ provision.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-sudo yum check-update
-sudo yum install git
-sudo yum install screen
+sudo yum install git screen wget
 sudo yum groupinstall "Development tools"
 sudo yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel atlas-devel
 
-cd $(mktemp)
+cd $(mktemp -d)
 wget http://python.org/ftp/python/2.7.8/Python-2.7.8.tgz
 tar xf Python-2.7.8.tgz
 cd Python-2.7.8
@@ -19,9 +17,6 @@ tar xf distribute-0.6.49.tar.gz
 cd distribute-0.6.49
 sudo /usr/local/bin/python2.7 setup.py install
 sudo /usr/local/bin/easy_install-2.7 pip
-sudo /usr/local/bin/pip-2.7 install whoosh
-sudo /usr/local/bin/pip-2.7 install numpy scipy scikit-learn nltk
-sudo python2.7 -c 'import nltk; nltk.download("all")'
 
 cat >/etc/yum.repos.d/docker.repo <<-EOF
 [dockerrepo]
@@ -35,4 +30,4 @@ EOF
 sudo yum check-update
 sudo yum install docker-engine
 
-sudo /usr/local/bin/pip-2.7 install docker-compose
+sudo /usr/local/bin/pip2.7 install docker-compose
